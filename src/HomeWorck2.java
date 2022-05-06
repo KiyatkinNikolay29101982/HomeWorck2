6f7e5eefded82c6bdd4158f3603d2bc5b6399d70
kiyat <kiyatkinnikolay@mail.ru>
2022-05-06 10:14:49 +0400

    init commit

---
+++ src/HomeWorck2.java
@@ -0,0 +1,37 @@
+// задача  из группы 224 в телеграмм заполните массив четными целыми числами,
+// начиная с числа 2 и выведите их на экран.
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class HomeWorck2 {
+
+
+
+        public static void main(String[] args){
+
+            Scanner sc = new Scanner(System.in);
+
+            System.out.println("введите верхнюю границу массива чисел");
+
+            int sizeOfArray = (int) sc.nextInt() / 2;
+
+            int [] evenNumber = new int[sizeOfArray];
+
+            for(int i = 0; i < sizeOfArray; i ++){
+                evenNumber[i] = (i + 1) * 2;
+            }
+            System.out.println("Содержание массива в строчку: ");
+            System.out.println(Arrays.toString(evenNumber));
+
+            System.out.println("содержание массива: ");
+
+            for (int i = 0; i < sizeOfArray; i ++ ){
+
+                System.out.printf(" %d %n", evenNumber[i]);
+            }
+
+        }
+
+    }
+
